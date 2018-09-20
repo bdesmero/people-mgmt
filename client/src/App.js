b1b971ceb21c3d056f4bccd2901177159d69e02e
bdesmero <bdesmero@gmail.com>
2018-09-20 21:38:54 +0800

    Try out tictactoe test app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './Game';
+
+const formatName = (user) => {
+  return user.firstName + ' ' + user.lastName;
+};
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
 
 class App extends Component {
   render() {
@@ -8,11 +18,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to eNPS</h1>
         </header>
+
+        <h1>Hello, {formatName(user)}!</h1>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Please give your rating
+          <input
+            type="text"
+            id="rating"
+            name="rating"
+          />
         </p>
+
+        <Game />
       </div>
     );
   }
